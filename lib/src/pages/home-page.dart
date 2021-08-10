21bff4589ebc3a8eb131902226fef8d01ddac731
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-10 09:36:02 -0500

    Drawers - Create a side menu

--- lib/src/pages/home-page.dart
+++ lib/src/pages/home-page.dart
@@ -1,3 +1,4 @@
+import 'package:app_preferences_user/src/pages/setting-page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,7 +11,9 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
           title: Text('Preferencias de usuario')
       ),
+      drawer: _crearMenu(context),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('Color secundario:'),
           Divider(),
@@ -18,6 +21,47 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre de usuario:'),
           Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _crearMenu( BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.fill
+              )
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.blue),
+            title: Text('Home'),
+            onTap: ()=> Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(Icons.api, color: Colors.blue),
+            title: Text('Api'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('People'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue),
+            title: Text('Settings'),
+            onTap: (){
+              Navigator.pushReplacementNamed(context, SettingPage.routeName);
+            },
+          )
         ],
       ),
     );
